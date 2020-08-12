2089bac50a8b85e41f3bbc82858b618eb4e136a2
linchuan <linc1982@icloud.com>
2020-08-12 10:37:27 +0800

    finish arraylist

--- code/2071/01-ArrayList/arrayList.c
+++ code/2071/01-ArrayList/arrayList.c
@@ -14,51 +14,62 @@ typedef struct {
 } SeqList;
 
 // 初始化顺序表
-SeqList* initSeqList(int cap) {
+SeqList* initSeqList() {
     SeqList* pl = (SeqList*)malloc(sizeof(SeqList));
-    pl->capacity = cap;
-    pl->head = (int *)malloc(sizeof(int) * cap);
+    pl->capacity = 10;
+    pl->head = (int *)malloc(sizeof(int) * 10);
     pl->length = 0;
     return pl;
 }
 
 // 增加数据
-void add(SeqList* pl, int value) {
-    if (pl->length < pl->capacity) {
-        pl->head[pl->length] = value;
-        pl->length++;
-    } else {
-        printf("容量已满\n");
+void add(SeqList* pl,int pos, int value) {
+    if (pl->length == pl->capacity) {
+        // 容量已满
+        pl->capacity <<= 1;
+        pl->head = (int *)realloc(pl->head, pl->capacity * sizeof(int));
     }
+
+    // 从length-1到pos的元素都往后复制一次
+    for (int i = pl->length; i > pos; i--) {
+        pl->head[i] = pl->head[i-1];
+    }
+
+    pl->head[pos] = value;
+    pl->length++;
 }
 
 // 删除数据
-void delete(SeqList* pl) {
-    if (pl->length != 0)
+void del(SeqList* pl, int pos) {
+    if (pos >= 0 && pos < pl->length) {
+        // 从pos到length-1的元素都往左移动一次
+        for (int i = pos; i < pl->length-1; i++) {
+            pl->head[i] = pl->head[i+1];
+        }
         pl->length--;
-    else
-        printf("空顺序表，无法删除\n");
+    } else {
+        printf("pos(%d)非法，当前length为%d\n", pos, pl->length);
+    }
 }
 
 void print(SeqList* pl) {
     for (int i = 0; i < pl->length; i++) {
-        printf("%4d", pl->head[i]);
+        printf("%5d", pl->head[i]);
     }
     printf("\n");
 }
 
 int main() {
-    SeqList* pl = initSeqList(10);
+    SeqList* pl = initSeqList();
     for (int i = 0; i < 20; i++) {
-        add(pl, i * 100);
+        add(pl, i, i * 100);
         print(pl);
     }
 
     for (int i = 0; i < 20; i++) {
-        delete(pl);
+        del(pl, pl->length-1);
         print(pl);
     }
-
 
     return 0;
 }
