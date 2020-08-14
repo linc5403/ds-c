a4828d1e7b549025f26607ebe3593fe3dec8c010
linchuan <linc1982@icloud.com>
2020-08-14 16:11:15 +0800

    增加逆波兰序表达式计算

--- code/2071/03-Stack/test.c
+++ code/2071/03-Stack/test.c
@@ -5,18 +5,63 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "LinkedStack.h"
 
-int main() {
-    Stack* s = initStack();
+// 判断s是否运算符
+int isOpertor(char* s) {
+    if (strlen(s) > 1) {
+        return 0;
+    }
+    // 长度都为1
+    switch (s[0]) {
+        case '*': case '/': case '+': case '-':
+            return 1;
+        default:
+            return 0;
+    }
+}
 
-    for (int i = 0; i < 10; i++) {
-        push(s, i);
+int evalRPN(char ** tokens, int tokensSize){
+    Stack* ps = initStack();
+    // 数字-> 入栈
+    // 符号-> 弹出1个作为右边的操作数, 再弹出一个作为左边的操作数
+    for (int i = 0; i < tokensSize; i++) {
+        // 判断tokens[i]是数字还是符号
+        if (isOpertor(tokens[i])) {
+            // 符号
+            int r = pop(ps);
+            int l = pop(ps);
+            int val;
+            switch (tokens[i][0]) {
+                case '+':
+                    val = r + l; break;
+                case '-':
+                    val = l - r; break;
+                case '*':
+                    val = l * r; break;
+                case '/':
+                    val = l / r; break;
+                default:
+                    printf("出错啦\n");
+                    return -100;
+            }
+            push(ps, val);
+        } else {
+            // 数字
+            push(ps, atoi(tokens[i]));
+        }
     }
-    while(!isEmpty(s)) {
-        printf("%3d", pop(s));
-    }
-    printf("\n");
+    return pop(ps);
+}
+
+
+int main() {
+    //char* a[] = {"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"};
+    char* a[] = {"4", "13", "5", "/", "+"};
+    int val = evalRPN(a, sizeof(a) / sizeof(a[0]));
+    printf("%d\n", val);
 }
 
